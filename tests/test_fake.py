2768665e908ea3737591f9839d86feb3f1743cd3
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-07-19 03:15:17 +0300

    Slider: Storage scope limit

--- tests/test_fake.py
+++ tests/test_fake.py
@@ -1,20 +1,26 @@
 import unittest
+from unittest.mock import Mock
 
-import sys
-print(sys.path)
-#import siv_modules
-from siv_modules import renderer
+from lib import renderer
 
 
 class Fake(unittest.TestCase):
-
     def test_fake(self):
-        self.assertEqual(7, 7)
+        mock = Mock()
+        list = [None, 1, 2, 3]
+        current = list.index(2)
+        def get_current(idx=0):
+            return list[current + idx]
+        mock.side_effect = get_current
+        print(mock(-1))
+        print(mock())
+        pass
 
     def test_import(self):
         sum = renderer.Sum(None, True, None, False)
         self.assertTrue(sum.left_done)
         self.assertFalse(sum.right_done)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tests/test_slider.py
@@ -0,0 +1,64 @@
+import unittest
+from unittest import mock
+import itertools
+
+
+from lib.slider import Slider
+
+
+class ToLeft(unittest.TestCase):
+
+    def setUp(self):
+        self.storage_mock = mock.Mock()
+        self.renderer_mock = mock.Mock()
+        self.ui_mock = mock.Mock()
+        self.instance = Slider(self.storage_mock, self.renderer_mock, self.ui_mock)
+
+    def test_create(self):
+        self.assertIsNotNone(self.instance)
+
+    def test_StorageLimit(self):
+        left_data = [None, object(), object()]
+        current_data = [object(), object()]
+        right_data = [object(), object(), None]
+
+        self.storage_mock.get_previous.side_effect = left_data[::-1]
+        self.storage_mock.get_current.side_effect = current_data
+        self.storage_mock.get_next.side_effect = right_data
+
+        expect_storage = (
+            mock.call.get_current(),
+            mock.call.get_previous(0), mock.call.get_previous(1), mock.call.get_previous(2),
+            mock.call.step_next(),
+            mock.call.get_current(),
+            mock.call.get_next(0), mock.call.get_next(1), mock.call.get_next(2)
+        )
+
+        calc_result = (
+            # left side
+            {'left': 0, 'left_done': False, 'right': 0, 'right_done': False},
+            {'left': 0, 'left_done': False, 'right': 1, 'right_done': False},
+            # right side
+            {'left': 0, 'left_done': False, 'right': 0, 'right_done': False},
+            {'left': 0, 'left_done': False, 'right': 1, 'right_done': False}
+        )
+
+        self.renderer_mock.calc.side_effect = calc_result
+
+        expect_renderer = itertools.chain(
+            (mock.call.calc(mock.ANY, mock.ANY) for dummy in range(len(calc_result))),
+            (mock.call.render(mock.ANY, mock.ANY, i) for i in range(0, 100))
+        )
+        expect_renderer = list(expect_renderer)
+
+        expect_ui = (mock.call.draw(mock.ANY) for dummy in range(0, 100))
+
+        self.instance.to_left()
+
+        self.storage_mock.assert_has_calls(expect_storage)
+        self.renderer_mock.assert_has_calls(expect_renderer)
+        self.ui_mock.assert_has_calls(expect_ui)
+
+
+if __name__ == '__main__':
+    unittest.main()
