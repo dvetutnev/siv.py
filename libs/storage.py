3dc9f1679d6f0fca57094c63fe86dda825469490
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-08-07 23:03:17 +0300

    Storage: get(0), first use

--- libs/storage.py
+++ libs/storage.py
@@ -8,29 +8,15 @@ class Storage(object):
     def __init__(self, path, extensions):
         self._path = Path(path)
         self._extensions = extensions
-        self._flist = None
 
-    def get_previous(self, offset):
+    def get(self, offest):
+        g = itertools.chain.from_iterable(self._path.glob(e) for e in self._extensions)
+        flist = sorted(g)
+        for fname in flist:
+            try:
+                img = Image.open(fname)
+                img.load()
+                return img
+            except:
+                continue
         return None
-
-    def get_current(self):
-        if self._flist is None:
-            g = itertools.chain.from_iterable(self._path.glob(e) for e in self._extensions)
-            self._flist = sorted(g)
-        if len(self._flist) == 0:
-            return None
-        fname = self._flist[0]
-        img = Image.open(fname)
-        img.load()
-        return img
-
-    def get_next(self, offset):
-        if self._flist is None:
-            g = itertools.chain.from_iterable(self._path.glob(e) for e in self._extensions)
-            self._flist = sorted(g)
-        if offset + 1 >= len(self._flist):
-            return None
-        fname = self._flist[offset + 1]
-        img = Image.open(fname)
-        img.load()
-        return img
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch, call, Mock
+from unittest.mock import patch, call, Mock, ANY
 
 
 from libs.storage import Storage, Path, Image
@@ -15,7 +15,7 @@ class CreateTest(unittest.TestCase):
         pass
 
 
-class Test(unittest.TestCase):
+class TestFirstUse(unittest.TestCase):
 
     def setUp(self):
         with patch('libs.storage.Path', name='Path_mock', autospec=True, spec_set=True) as Path_mock:
@@ -23,87 +23,70 @@ class Test(unittest.TestCase):
             Path_mock.return_value = self.path_mock
             self.instance = Storage('/home/user/pictures', ['*.jpg', '*.png'])
 
-    def test_first_get_current_None(self):
+    def test_get_0_None(self):
         glob_mock = self.path_mock.glob
         glob_mock.side_effect = [[], []]
 
-        result = self.instance.get_current()
+        result = self.instance.get(0)
 
         self.assertIsNone(result)
         glob_mock.assert_has_calls([call('*.jpg'), call('*.png')])
 
     @patch('libs.storage.Image', name='Image_mock', autospec=True, spec_set=True)
-    def test_first_get_current(self, Image_mock):
+    def test_get_0_Normal(self, Image_mock):
         glob_mock = self.path_mock.glob
         glob_mock.side_effect = [['3.jpg', '1.jpg'], ['2.png', '4.png']]
+
         img_mock = Mock(name='img_mock', spec_set=Image.Image)
         Image_mock.open.return_value = img_mock
+        expect_open = [call.open('1.jpg')]
 
-        result = self.instance.get_current()
+        result = self.instance.get(0)
 
-        self.assertEqual(result, img_mock)
-        Image_mock.open.assert_called_once_with('1.jpg')
+        Image_mock.assert_has_calls(expect_open)
         img_mock.load.assert_called_once_with()
+        self.assertEqual(result, img_mock)
 
-    def test_first_get_previous_None(self):
+    @patch('libs.storage.Image', name='Image_mock', autospec=True, spec_set=True)
+    def test_get_0_AllBadImage(self, Image_mock):
         glob_mock = self.path_mock.glob
         glob_mock.side_effect = [['3.jpg', '1.jpg'], ['2.png', '4.png']]
 
-        result = self.instance.get_previous(0)
+        Image_mock.open.side_effect = FileNotFoundError('Mock: not found')
+        expect_open = [call.open('1.jpg'), call.open('2.png'), call.open('3.jpg'), call.open('4.png')]
+
+        result = self.instance.get(0)
+
+        Image_mock.assert_has_calls(expect_open)
         self.assertIsNone(result)
 
     @patch('libs.storage.Image', name='Image_mock', autospec=True, spec_set=True)
-    def test_first_get_next(self, Image_mock):
+    def test_get_0_SkipBadImage(self, Image_mock):
         glob_mock = self.path_mock.glob
         glob_mock.side_effect = [['3.jpg', '1.jpg'], ['2.png', '4.png']]
-        imgs = [
-            Mock(name='img_2_mock', spec_set=Image.Image),
-            Mock(name='img_3_mock', spec_set=Image.Image),
-            Mock(name='img_4_mock', spec_set=Image.Image)
-        ]
-        Image_mock.open.side_effect = imgs
 
-        img = self.instance.get_next(0)
-        self.assertEqual(img, imgs[0])
-        img = self.instance.get_next(1)
-        self.assertEqual(img, imgs[1])
-        img = self.instance.get_next(2)
-        self.assertEqual(img, imgs[2])
-        img = self.instance.get_next(3)
-        self.assertIsNone(img)
+        img_bad = Mock(name='img_mock', spec_set=Image.Image)
+        img_bad.load.side_effect = OSError('Mock: format invalid')
+        img_normal = Mock(name='img_normal', spec_set=Image.Image)
 
-        glob_mock.assert_has_calls([call('*.jpg'), call('*.png')])
-        Image_mock.open.assert_has_calls([call('2.png'), call('3.jpg'), call('4.png')])
-        imgs[0].load.assert_called_once_with()
-        imgs[1].load.assert_called_once_with()
-        imgs[2].load.assert_called_once_with()
+        def _open(fname):
+            if not hasattr(_open, 'count'):
+                _open.count = 0
+                raise FileNotFoundError('Mock: ' + fname + ' not found')
+            if _open.count == 0:
+                _open.count = 1
+                return img_bad
+            if _open.count == 1:
+                return img_normal
+        Image_mock.open.side_effect = _open
+        expect_open = [call.open('1.jpg'), call.open('2.png'), call.open('3.jpg')]
 
-    @patch('libs.storage.Image', name='Image_mock', autospec=True, spec_set=True)
-    def test_get_next_after_get_current(self, Image_mock):
-        glob_mock = self.path_mock.glob
-        glob_mock.side_effect = [['3.jpg', '1.jpg'], ['2.png', '4.png']]
-        imgs = [
-            Mock(name='img_1_mock', spec_set=Image.Image),
-            Mock(name='img_2_mock', spec_set=Image.Image),
-        ]
-        Image_mock.open.side_effect = imgs
+        result = self.instance.get(0)
 
-        img_current = self.instance.get_current()
-        self.assertEqual(img_current, imgs[0])
-        img_next = self.instance.get_next(0)
-        self.assertEqual(img_next, imgs[1])
-
-        glob_mock.assert_has_calls([call('*.jpg'), call('*.png')])
-        Image_mock.open.assert_has_calls([call('1.jpg'), call('2.png')])
-        imgs[0].load.assert_called_once_with()
-        imgs[1].load.assert_called_once_with()
-
-    def test_first_get_next_None(self):
-        glob_mock = self.path_mock.glob
-        glob_mock.side_effect = [['1.jpg'], []]
-
-        result = self.instance.get_next(0)
-        self.assertIsNone(result)
+        Image_mock.assert_has_calls(expect_open)
+        img_bad.load.assert_called_once_with()
+        img_normal.load.assert_called_once_with()
+        self.assertEqual(result, img_normal)
 
 
 if __name__ == '__main__':
